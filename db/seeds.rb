e5005daa2820cafab340c193755c0e0c0064dde8
Nabil Ghachem <gach.nabil@gmail.com>
2025-02-26 14:55:12 +0100

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "cleaning data base..."
+Restaurant.destroy_all
+puts "creating databse..."
+Restaurant.create!(name: "Epicure", address: "75008 Paris", phone_number: "01 43 54 23 31", category: "french")
+Restaurant.create!(name: "Sushi Shop", address: "75009 Paris", phone_number: "01 42 34 54 76", category: "japanese")
+Restaurant.create!(name: "Pizza Hut", address: "75010 Paris", phone_number: "01 45 67 89 10", category: "italian")
+Restaurant.create!(name: "Dumpling House", address: "75011 Paris", phone_number: "01 23 45 67 89", category: "chinese")
+Restaurant.create!(name: "Friterie Belge", address: "75012 Paris", phone_number: "01 98 76 54 32", category: "belgian")
+
+puts "Seed completed"
